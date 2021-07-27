81e62327be07db2bb3a4715b1a982b909d6e5daa
cirodelia74 <85739966+cirodelia74@users.noreply.github.com>
2021-07-27 16:32:53 +0200

    Update CodiceFiscaleGuiV1Tester.java

--- CodiceFiscaleGuiV1Tester.java
+++ CodiceFiscaleGuiV1Tester.java
@@ -9,7 +9,16 @@ public class CodiceFiscaleGuiV1Tester {
 
 	public static void main(String[] args) {
 		
+		/*
+		 * Applicazione con inserimento della data di nascita da tastiera per mezzo
+		 * di 3 JTextField
+		 */
 		// new CodiceFiscaleGuiV1();
+		
+		/*
+		 * Applicazione con inserimento della data di nascita attraverso la 
+		 * selezione dei valori con 3 JComboBox
+		 */
 		new CodiceFiscaleGuiV1Combo();
 
 	}
